9994fc070d043bd30e35aaf88b31898306de1d68
Adam <94743466+adam-dziuk@users.noreply.github.com>
2026-09-21 17:22:19 +0200

    Change Author tagline to 'custom IT systems'

--- hermes-seo-abilities.php
+++ hermes-seo-abilities.php
@@ -5,7 +5,7 @@
  * Version:           1.2.0
  * Requires at least: 6.9
  * Requires PHP:      7.4
- * Author:            DD Systems - dedicated IT systems
+ * Author:            DD Systems - custom IT systems
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       hermes-seo
